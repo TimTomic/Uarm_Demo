bf6c67ba66bd59ef968e8ef8bb882b97b0682e12
Tim Lachmann <timlachmann56@gmail.com>
2026-03-03 21:49:35 +0100

    Include swiftpro launch in arm_controller.launch.py

--- src/uarm_arm_controller/launch/arm_controller.launch.py
+++ src/uarm_arm_controller/launch/arm_controller.launch.py
@@ -3,6 +3,9 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 def generate_launch_description():
     poses_config_file = os.path.join(
         get_package_share_directory('uarm_arm_controller'),
@@ -18,6 +21,12 @@ def generate_launch_description():
         parameters=[poses_config_file]
     )
 
+    swiftpro_launch_dir = os.path.join(get_package_share_directory('swiftpro'), 'launch')
+    swiftpro_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(swiftpro_launch_dir, 'pro_control.launch.py'))
+    )
+
     return LaunchDescription([
+        swiftpro_launch,
         arm_controller_node
     ])
